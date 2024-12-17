0014f971a2a7e5b7e74778c804a79f3ce6220ae1
Tosu <terebina860@gmail.com>
2024-12-18 04:26:24 +0530

    Update admins.py

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -23,8 +23,8 @@ from pyrogram.types import Message
 from DaisyXMusic.function.admins import set
 from DaisyXMusic.helpers.channelmusic import get_chat_id
 from DaisyXMusic.helpers.decorators import authorized_users_only, errors
-from DaisyXMusic.helpers.filters import command
-from DaisyXMusic.services.callsmusic import callsmusic, other_filters
+from DaisyXMusic.helpers.filters import command, other_filters
+from DaisyXMusic.services.callsmusic import callsmusic
 from DaisyXMusic.services.queues import queues
 from DaisyXMusic.config import que
 
