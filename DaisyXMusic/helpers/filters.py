7ebc0d0625e4f0ab94999b67c962321701d972c1
Tosu <terebina860@gmail.com>
2024-12-18 04:00:03 +0530

    Update filters.py

--- DaisyXMusic/helpers/filters.py
+++ DaisyXMusic/helpers/filters.py
@@ -23,22 +23,6 @@ from DaisyXMusic.config import COMMAND_PREFIXES
 
 
 
-from pyrogram import Client, filters
-
-# Define the function and apply the decorator to it
-@Client.on_message(filters.group & ~filters.via_bot & ~filters.forwarded)
-async def handle_group_message(client, message):
-    # Your logic here
-    pass
-
-
-from pyrogram import Client, filters
-
-       @Client.on_message(filters.private & ~filters.via_bot & ~filters.forwarded)
-async def handle_message(client, message):
-    if message.edit_date:  # Checks if the message has been edited
-        # Your logic for edited messages
-        pass
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
