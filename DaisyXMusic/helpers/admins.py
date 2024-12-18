a47396980f55eca9ecde1db0ff409287ba92245e
Tosu <terebina860@gmail.com>
2024-12-18 14:39:21 +0530

    Update admins.py

--- DaisyXMusic/helpers/admins.py
+++ DaisyXMusic/helpers/admins.py
@@ -9,19 +9,3 @@ async def jiosaavn(client, message):
     # Ensure you are using the correct variable for the chat
     administrators = await get_administrators(message.chat)
     # Rest of your function logic
-
-async def get_administrators(chat: Chat) -> List[int]:
-    get = gett(chat.id)
-
-    if get:
-        return get
-    else:
-        administrators = await chat.get_members(filter="administrators")
-        to_set = []
-
-        for administrator in administrators:
-            if administrator.can_manage_voice_chats:
-                to_set.append(administrator.user.id)
-
-        set(chat.id, to_set)
-        return await get_administrators(chat)
