9fde2c860744d81c73b957648f152d81c4664094
Tosu <terebina860@gmail.com>
2024-12-18 14:36:30 +0530

    Update admins.py

--- DaisyXMusic/helpers/admins.py
+++ DaisyXMusic/helpers/admins.py
@@ -4,6 +4,11 @@ from pyrogram.types import Chat
 from DaisyXMusic.function.admins import get as gett
 from DaisyXMusic.function.admins import set
 
+# In modules/play.py
+async def jiosaavn(client, message):
+    # Ensure you are using the correct variable for the chat
+    administrators = await get_administrators(message.chat)
+    # Rest of your function logic
 
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
